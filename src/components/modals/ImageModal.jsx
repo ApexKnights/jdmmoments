dffae5bb2cf8b37110b12f10d782753ec08da623
Souparno <souparna2000shee@gmail.com>
2024-09-12 17:31:30 +0530

    minor adds

--- src/components/modals/ImageModal.jsx
+++ src/components/modals/ImageModal.jsx
@@ -7,7 +7,7 @@ const ImageModal = ({ mimg, setImgModal, modalColor, free }) => {
         <div className='fixed z-10 top-[0] left-[0] w-[100%] h-[100%]   flex items-center justify-center'>
 
             <div className={`w-[90%] h-50% md:h-[90%] md:w-[70%]  flex items-center justify-center bg-[${modalColor}] relative`}>
-                <div className='absolute top-2 right-2 z-10' onClick={() => setImgModal(false)}><RxCross2 className='text-[24px] text-white' /></div>
+                <div className='absolute top-2 right-2 z-10' onClick={() => setImgModal(false)}><RxCross2 className='text-[24px] text-black sm:text-white' /></div>
                 {
                     !free ? <div className=' absolute z-10  bg-white w-[70%] py-6 px-2 text-center '>
                         <h2 className='text-[30px] font-[800]'>Buy this product, To see the full image</h2>
